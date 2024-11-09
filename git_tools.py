b3c8e46b060da0de591c4d84f2eb8598b189217b
user.name <user.email>
2024-11-09 16:12:58 +0200

    Add get_commit_timestamp, Fix projects_to_git_urls

--- git_tools.py
+++ git_tools.py
@@ -1,14 +1,27 @@
+from datetime import datetime
 from pathlib import Path
 from pydriller import Repository
 from git import Repo
-
+from constants import *
 
 def projects_to_git_urls(projects: set[str]) -> set[str]:
     """Transform apache project names to GitHub repository links."""
-    return {
-        f"https://github.com/apache/{p.split("_", maxsplit=1)[1]}.git"
-        for p in projects
-    }
+    project_urls: str[str] = set()
+    for p in projects:
+        if p.startswith("apache_"):
+            p = p.split("_", maxsplit=1)[1]
+        if p.startswith("apache-"):
+            p = p.split("-", maxsplit=1)[1]
+        if "log4cxx" in p:
+            p = f"logging-{p}"
+        if any(name in p for name in ("knox", "jspwiki", "ant-master", "nemo", "milagro", "pdfbox", "poi-parent", "roller-master")):
+            p = p.split("-", maxsplit=1)[0]
+            if ":" in p:
+                p = "incubator-" + p.split(":", maxsplit=1)[1]
+            if "milagro" in p:
+                p = "incubator-" + p
+        project_urls.add(f"https://github.com/apache/{p}.git")
+    return project_urls
 
 
 def clone_repositories(repository_urls: list[str], directory: Path) -> list[Repo]:
@@ -27,3 +40,10 @@ def clone_repositories(repository_urls: list[str], directory: Path) -> list[Repo
         print(f"Skipping, repository already cloned into {subdir}")
         repos.append(Repo(subdir))
     return repos
+
+
+def get_commit_timestamp(repo_name: str, commit_sha1: str) -> datetime:
+    repo_path = git_clones_dir / repo_name
+    repository = Repo(repo_path)
+    commit = repository.commit(commit_sha1)
+    return commit.committed_datetime
