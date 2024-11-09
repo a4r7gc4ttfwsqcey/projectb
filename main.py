569ba44e37dd403fe71b440030594653f0aa5620
user.name <user.email>
2024-11-09 11:24:58 +0200

    Add docstrings to main.py

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from constants import *
 from csv_tools import parse_projects_from_csv
 
 def build_refactoringminer() -> None:
+    """Build refactoringminer from source with Gradle."""
     run_subprocess([str(gradle_exec), "jar"], cwd=rf_miner_dir)
 
 
 def setup_tools() -> None:
+    """Set up the required mining tools."""
     build_refactoringminer()
 
 
 def main() -> None:
+    """Main function to orchestrate all the steps."""
     setup_tools()
     projects = parse_projects_from_csv(input_csv)
     git_urls = projects_to_git_urls(projects)
