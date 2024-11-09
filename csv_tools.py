5e8fc242babdd139880592ac3a55b996aa308577
user.name <user.email>
2024-11-09 11:23:57 +0200

    Add docstring for parse_projects_from_csv

--- csv_tools.py
+++ csv_tools.py
@@ -2,5 +2,6 @@ import csv
 from pathlib import Path
 
 def parse_projects_from_csv(path: Path) -> set[str]:
+    """Get the unique projects from the project column rows in the input csv file."""
     with path.open("r", encoding="utf-8") as file:
         return {row["project"] for row in csv.DictReader(file)}
