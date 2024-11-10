cebbf961359268ddd063e1dd413c8f43ce595ff5
user.name <user.email>
2024-11-10 19:48:28 +0200

    fix extra await

--- csv_tools.py
+++ csv_tools.py
@@ -11,7 +11,7 @@ async def parse_projects_from_csv(path: Path) -> set[str]:
 async def write_table_to_csv(path: Path, table_dict: dict[str, list[str]]) -> bool:
     """Save table columns, rows from a dictionary into a csv file."""
     with path.open("w", encoding="utf-8") as file:
-        writer = csv.DictWriter(await file, fieldnames=table_dict.keys())
+        writer = csv.DictWriter(file, fieldnames=table_dict.keys())
         writer.writeheader()
         rows = [dict(zip(table_dict, t)) for t in zip(*table_dict.values())]
         writer.writerows(rows)
